07dd2fb7dfc69b00b16ea4c8dba90d295197e201
Josué Estrada <dev.josuestrd@gmail.com>
2023-07-22 21:57:54 -0500

    Add more files to exclude in .prettierignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
-      <span>
+      <h1 className="text-center text-lg">Hello World!</h1>
+      <span className="text-center text-sm">
         Vite + React + TailwindCSS + Eslint Airbnb + Prettier +
         Prettier-Tailwind
       </span>
-      <div className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">
-        hpña
-      </div>
     </>
   );
 }
